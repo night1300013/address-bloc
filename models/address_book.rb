b90c828d3531c15cc0e8c9a00af8cebc8ba10dcd
ShangMing Cherng <night1300013@gmail.com>
2017-12-31 21:12:00 -0800

    assignment-7-searching:completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -43,7 +43,16 @@ class AddressBook
         lower = mid + 1
       end
     end
-    
+
+    return nil
+  end
+
+  def iterative_search(name)
+    entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
     return nil
   end
 end
